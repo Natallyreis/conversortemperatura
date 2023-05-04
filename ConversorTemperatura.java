140dfb100103746cecd616608529244b1ed5effb
Natallyreis <130668435+Natallyreis@users.noreply.github.com>
2023-05-04 12:04:52 -0300

    comiiti

--- ConversorTemperatura.java
+++ ConversorTemperatura.java
@@ -1,1 +1,19 @@
-public
+import java.util.Scanner;
+
+public class ConversorTemperatura{
+    public static void main(String[] args){
+        //Declaração de variaveis
+        double c, f, total;
+
+        //Criação e instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("\n\t\t\t -- Conversor Simples --\n");
+            System.out.println("1. Celsius");
+            System.out.println("2. Fahren");
+            System.out.println("3. Multiplicação");
+
+
+
+    }
+}
